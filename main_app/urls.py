7aed02192774d4dd13e19e3056714bb4496d51d7
sabrinaaziz13 <sabrinaaziz95@gmail.com>
2023-07-16 21:43:34 -0400

    added nav bar and urls. & added static files

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-# this like app.use() in express
 urlpatterns = [
   path('', views.Home.as_view(), name="home"),
   path('about/', views.About.as_view(), name="about"),
+  path('casting-directors/', views.CastingDirector.as_view(), name="casting_director_list"),
+  path('profile/', views.Profile.as_view(), name="profile"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -11,8 +11,22 @@ class About(View):
     def get(self, request):
         return HttpResponse("The Network About Page")
     
+class CastingDirector(View):
+    def get(self, request):
+        return HttpResponse("The Network's Casting Directors")
+    
+class Profile(View):
+    def get(self, request):
+        return HttpResponse("Your Profile")
+    
 class Home(TemplateView):
     template_name = "home.html"
 
 class About(TemplateView):
     template_name = "about.html"
+
+class CastingDirector(TemplateView):
+    template_name = "casting_director_list.html"
+
+class Profile(TemplateView):
+    template_name = "profile.html"
